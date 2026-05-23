4f52c4c6dbc4e02d72a538f77ca56c1ca14331cd
Charfeddine856 <53520270+Charfeddine856@users.noreply.github.com>
2026-05-23 08:17:39 +0100

    Maximize niche defaults with extensive web source coverage

--- src/NicheManager.php
+++ src/NicheManager.php
@@ -66,7 +66,21 @@ class NicheManager
                 'name' => 'General Automotive',
                 'description' => 'General car news and reviews.',
                 'rss' => [
-                    'https://www.caranddriver.com/rss/all.xml'
+                    'https://www.caranddriver.com/rss/all.xml',
+                    'https://www.autoblog.com/rss.xml',
+                    'https://www.motortrend.com/feeds/all/',
+                    'https://www.thetruthaboutcars.com/feed/',
+                    'https://www.carscoops.com/feed/',
+                    'https://www.autocar.co.uk/rss',
+                    'https://www.topgear.com/car-news/rss.xml',
+                    'https://www.carmagazine.co.uk/rss/',
+                    'https://www.roadandtrack.com/rss/all.xml',
+                    'https://www.thedrive.com/rss/all',
+                    'https://jalopnik.com/rss',
+                    'https://www.autoevolution.com/rss.xml',
+                    'https://www.cars.com/news/rss/',
+                    'https://www.edmunds.com/feeds/rss/reviews.xml',
+                    'https://www.whichcar.com.au/rss.xml'
                 ],
                 'web' => [
                     'https://www.autoblog.com/news/'
@@ -76,38 +90,167 @@ class NicheManager
                 'name' => 'Electric Vehicles',
                 'description' => 'EV news, reviews and charging guides.',
                 'rss' => [
-                    'https://insideevs.com/rss'
+                    'https://insideevs.com/rss',
+                    'https://electrek.co/feed/',
+                    'https://cleantechnica.com/tag/electric-vehicles/feed/',
+                    'https://evannex.com/blogs/news.atom',
+                    'https://chargedevs.com/feed/',
+                    'https://www.greencarreports.com/rss',
+                    'https://www.teslarati.com/feed/',
+                    'https://www.ev-database.org/rss',
+                    'https://www.autocar.co.uk/car-news/electric-cars/rss',
+                    'https://www.carscoops.com/tag/electric-cars/feed/',
+                    'https://electrive.com/feed/',
+                    'https://evmagz.com/feed/',
+                    'https://pluginamerica.org/blog/feed/',
+                    'https://chargedevs.com/newswire/feed/',
+                    'https://www.energy.gov/eere/electricvehicles/rss.xml'
                 ],
                 'web' => [
-                    'https://insideevs.com/news/'
+                    'https://insideevs.com/news/',
+                    'https://electrek.co/',
+                    'https://www.greencarreports.com/news',
+                    'https://www.teslarati.com/',
+                    'https://www.autocar.co.uk/car-news/electric-cars',
+                    'https://www.carscoops.com/tag/electric-cars/',
+                    'https://cleantechnica.com/tag/electric-vehicles/',
+                    'https://chargedevs.com/newswire/',
+                    'https://electrive.com/',
+                    'https://evmagz.com/'
                 ]
             ],
             'motorcycles' => [
                 'name' => 'Motorcycles',
                 'description' => 'Motorcycle news and reviews.',
                 'rss' => [
-                    'https://www.motorcyclenews.com/rss/'
+                    'https://www.motorcyclenews.com/rss/',
+                    'https://www.visordown.com/rss.xml',
+                    'https://www.rideapart.com/rss/',
+                    'https://www.cycleworld.com/arc/outboundfeeds/rss/',
+                    'https://www.motorcycle.com/feeds/all/',
+                    'https://www.bennetts.co.uk/bikesocial/rss',
+                    'https://www.advrider.com/feed/',
+                    'https://www.revzilla.com/common-tread/rss',
+                    'https://www.webbikeworld.com/feed/',
+                    'https://www.motousher.com/feed/',
+                    'https://www.motorcyclecruiser.com/feed/',
+                    'https://www.advpulse.com/feed/',
+                    'https://www.bikeexif.com/feed',
+                    'https://www.returnofthecaferacers.com/feed/',
+                    'https://www.totalmotorcycle.com/feed/'
                 ],
-                'web' => []
+                'web' => [
+                    'https://www.motorcyclenews.com/news/',
+                    'https://www.visordown.com/news',
+                    'https://www.rideapart.com/news/',
+                    'https://www.cycleworld.com/motorcycle-news/',
+                    'https://www.motorcycle.com/news',
+                    'https://www.bennetts.co.uk/bikesocial/news-and-views',
+                    'https://www.advrider.com/f/',
+                    'https://www.revzilla.com/common-tread',
+                    'https://www.webbikeworld.com/',
+                    'https://www.advpulse.com/'
+                ]
             ],
 
             'auto-mobile' => [
                 'name' => 'Auto Mobile',
                 'description' => 'Automotive mobile trends, cars and transport updates.',
-                'rss' => [],
-                'web' => []
+                'rss' => [
+                    'https://www.autonews.com/section/rss',
+                    'https://www.carwow.co.uk/blog/rss.xml',
+                    'https://www.whatcar.com/news/rss',
+                    'https://www.driving.co.uk/feed/',
+                    'https://www.carsguide.com.au/news/rss',
+                    'https://www.autoexpress.co.uk/rss.xml',
+                    'https://www.cnet.com/roadshow/news/rss/',
+                    'https://www.arenaev.com/rss-news-reviews.php3',
+                    'https://www.techradar.com/rss/news/car-tech',
+                    'https://www.wired.com/feed/tag/transport/latest/rss',
+                    'https://www.theverge.com/rss/transportation/index.xml',
+                    'https://www.engadget.com/transportation/rss.xml',
+                    'https://www.digitaltrends.com/cars/feed/',
+                    'https://arstechnica.com/cars/feed/',
+                    'https://www.zdnet.com/topic/transportation/rss.xml'
+                ],
+                'web' => [
+                    'https://www.autonews.com/',
+                    'https://www.carwow.co.uk/news',
+                    'https://www.whatcar.com/news',
+                    'https://www.autoexpress.co.uk/car-news',
+                    'https://www.carsguide.com.au/car-news',
+                    'https://www.cnet.com/roadshow/',
+                    'https://www.digitaltrends.com/cars/',
+                    'https://www.theverge.com/transportation',
+                    'https://arstechnica.com/cars/',
+                    'https://www.techradar.com/news/car-tech'
+                ]
             ],
             'cuisine' => [
                 'name' => 'Cuisine',
                 'description' => 'Food, recipes, and restaurant-related content.',
-                'rss' => [],
-                'web' => []
+                'rss' => [
+                    'https://www.seriouseats.com/rss',
+                    'https://www.bonappetit.com/feed/rss',
+                    'https://www.epicurious.com/services/rss/feeds/all',
+                    'https://www.foodnetwork.com/content/food-com/en/rss/all-content.rss',
+                    'https://www.simplyrecipes.com/feed/',
+                    'https://www.delish.com/rss/all.xml',
+                    'https://www.thekitchn.com/rss',
+                    'https://minimalistbaker.com/feed/',
+                    'https://cookieandkate.com/feed/',
+                    'https://www.smittenkitchen.com/feed/',
+                    'https://www.foodandwine.com/feed',
+                    'https://www.allrecipes.com/feed/',
+                    'https://www.loveandlemons.com/feed/',
+                    'https://www.feastingathome.com/feed/',
+                    'https://www.halfbakedharvest.com/feed/'
+                ],
+                'web' => [
+                    'https://www.seriouseats.com/',
+                    'https://www.bonappetit.com/',
+                    'https://www.epicurious.com/',
+                    'https://www.foodnetwork.com/',
+                    'https://www.simplyrecipes.com/',
+                    'https://www.delish.com/',
+                    'https://www.thekitchn.com/',
+                    'https://minimalistbaker.com/',
+                    'https://cookieandkate.com/',
+                    'https://www.foodandwine.com/'
+                ]
             ],
             'eran-money' => [
                 'name' => 'Eran Money',
                 'description' => 'Business, money and personal finance content.',
-                'rss' => [],
-                'web' => []
+                'rss' => [
+                    'https://www.investopedia.com/feedbuilder/feed/getfeed?feedName=rss_articles',
+                    'https://www.nerdwallet.com/blog/feed/',
+                    'https://www.marketwatch.com/rss/topstories',
+                    'https://www.cnbc.com/id/100003114/device/rss/rss.html',
+                    'https://www.ft.com/?format=rss',
+                    'https://www.economist.com/finance-and-economics/rss.xml',
+                    'https://www.fool.com/feeds/index.aspx',
+                    'https://www.kiplinger.com/rss.xml',
+                    'https://www.moneycrashers.com/feed/',
+                    'https://www.businessinsider.com/rss',
+                    'https://www.wsj.com/xml/rss/3_7031.xml',
+                    'https://www.bloomberg.com/feed/podcast/etf-report.xml',
+                    'https://feeds.a.dj.com/rss/RSSMarketsMain.xml',
+                    'https://www.forbes.com/money/feed/',
+                    'https://www.morningstar.com/feeds/rss/articles'
+                ],
+                'web' => [
+                    'https://www.investopedia.com/',
+                    'https://www.nerdwallet.com/',
+                    'https://www.marketwatch.com/',
+                    'https://www.cnbc.com/personal-finance/',
+                    'https://www.ft.com/markets',
+                    'https://www.economist.com/finance-and-economics',
+                    'https://www.fool.com/',
+                    'https://www.kiplinger.com/',
+                    'https://www.moneycrashers.com/',
+                    'https://www.forbes.com/money/'
+                ]
             ]
         ];
 
